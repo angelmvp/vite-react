99c57cdf8ec0cbb0cd58243700ac626aeca7b010
MVP <vazquezperezangelmartin27@gmail.com>
2025-12-16 16:02:01 -0600

    uthink is good

--- src/api/speectAPI.ts
+++ src/api/speectAPI.ts
@@ -1,5 +1,5 @@
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://huggingface.co/spaces/avazquez112/SpeechSynthesis";
 
 export const api = {
     get: async (endpoint: string) => {
